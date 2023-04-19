aa5d579873c56b233c7771ef380b288f7a925daa
Pavel Chen <you@example.com>
2023-04-19 23:24:57 +1000

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import openai
 
+# Подключаем api ключ
+openai.api_key = "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"
 
 app = Flask(__name__)
 client = MongoClient("mongodb+srv://pavel4en:fjzQDT4g7vOTRhLD@gugo.dzfexwi.mongodb.net/?retryWrites=true&w=majority")
@@ -11,6 +14,7 @@ db = client['ToDo_db']
 tasks = db.tasks
 # Создаем коллекцию выполненных задач
 archive_tasks = db.archive_tasks
+
 
 
 @app.route('/')
@@ -24,9 +28,38 @@ def add_task():
     # Получаем данные из формы
     task_name = request.form['task_name']
     task_description = request.form['task_description']
-    # Добавляем задачу в коллекцию
-    tasks.insert_one({'name': task_name, 'description': task_description, 'completed': False})
-    return redirect(url_for('index'))
+
+    # Получаем оценку сложности задачи с помощью нейросети
+    model_engine = "text-davinci-003"
+    prompt = f"What is the difficulty level of the task '{task_name}' on a scale of 1 to 5?"
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=60,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # Обрабатываем ошибки
+    if response.choices[0].text.strip() not in ['1', '2', '3', '4', '5']:
+        return "Не возможно определить уровень сложности задачи"
+    else:
+        difficulty = int(response.choices[0].text.strip())
+        if difficulty == 1:
+            coins = 50
+        elif difficulty == 2:
+            coins = 100
+        elif difficulty == 3:
+            coins = 300
+        elif difficulty == 4:
+            coins = 500
+        else:
+            coins = 1000
+
+        # Добавляем задачу в коллекцию
+        tasks.insert_one({'name': task_name, 'description': task_description, 'completed': False, 'difficulty': difficulty, 'coins': coins})
+        return redirect(url_for('index'))
 
 
 @app.route('/edit_task/<task_id>', methods=['GET', 'POST'])
@@ -35,9 +68,43 @@ def edit_task(task_id):
     task = tasks.find_one({'_id': ObjectId(task_id)})
     if request.method == 'POST':
         # Обновляем данные задачи
-        tasks.update_one({'_id': ObjectId(task_id)}, {'$set': {'name': request.form['task_name'], 'description': request.form['task_description']}})
+        task_name = request.form['task_name']
+        task_description = request.form['task_description']
+
+        # Оцениваем сложность задачи и вычисляем количество монет
+        model_engine = "text-davinci-003"
+        prompt = f"What is the difficulty level of the task '{task_name}' on a scale of 1 to 5?"
+        response = openai.Completion.create(
+            engine=model_engine,
+            prompt=prompt,
+            max_tokens=60,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+
+        # Обрабатываем ошибки
+        if response.choices[0].text.strip() not in ['1', '2', '3', '4', '5']:
+            return "Не возможно определить уровень сложности задачи"
+        else:
+            difficulty = int(response.choices[0].text.strip())
+            if difficulty == 1:
+                coins = 50
+            elif difficulty == 2:
+                coins = 100
+            elif difficulty == 3:
+                coins = 300
+            elif difficulty == 4:
+                coins = 500
+            else:
+                coins = 1000
+
+        # Обновляем данные задачи в базе данных
+        tasks.update_one({'_id': ObjectId(task_id)}, {'$set': {'name': task_name, 'description': task_description, 'difficulty': difficulty, 'coins': coins}})
         return redirect(url_for('index'))
     return render_template('edit_task.html', task=task)
+
+
 
 
 @app.route('/delete_task/<task_id>', methods=['POST'])
