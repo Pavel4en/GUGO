811de68ccbd784724f2221247ae41e8d6f5384f0
ultragreed <agent002004@gmail.com>
2023-04-23 16:54:25 +1000

    fixed css

--- react-app/src/todo.tsx
+++ react-app/src/todo.tsx
@@ -2,42 +2,50 @@ import React, {useState} from 'react';
 import './todo.css';
 
 
-interface ITask {
-    initTaskName: string,
-    initTaskDescription: string,
-    initTaskID: string
+const TodoApp = () => {
+    const [tasksList, setTasksList] = useState<JSX.Element[]>([]);
+
+    const addTask = (task: JSX.Element) => {
+        setTasksList([
+            ...tasksList,
+            task
+        ]);
+    }
+
+    const deleteTask = (task: JSX.Element) => {
+
+    }
+
+    const editTast = (task: JSX.Element) => {
+
+    }
+
+    return (
+        <div className="todo_app">
+            <TaskManageBar
+                addTask={addTask}/>
+            <TaskList
+                tasksList={tasksList}/>
+        </div>
+    );
 }
 
-interface ITasksArr {
-    tasksList: JSX.Element[]
-}
 
 interface IAddTask {
     addTask: (task: JSX.Element) => void
 }
-
-const TaskListElement: (a: ITask) => JSX.Element = ({initTaskName, initTaskDescription, initTaskID}: ITask) => {
-    const [taskID, setTaskID] = useState(initTaskID);
-    const [taskName, setTaskName] = useState(initTaskName);
-    const [taskDescription, setTaskDescription] = useState(initTaskDescription);
-
+const TaskManageBar = ({addTask}: {addTask: (task: JSX.Element) => void}) => {
     return (
-        <div className="task_list_element">
-            <div className="content">
-                <input type="text" className="text" value={taskID} readOnly/>
-                <input type="text" className="text" value={taskName} readOnly/>
-                <input type="text" className="text" value={taskDescription} readOnly/>
-            </div>
-            <div className="actions">
-                <button className="edit">edit</button>
-                <button className="delete">delete</button>
-            </div>
+        <div className="task_manage_bar">
+            <TaskSearchForm/>
+            <TaskAddForm
+                addTask={addTask}/>
         </div>
     );
-
 }
 
-const TaskAddingForm = ({addTask}: IAddTask) => {
+
+const TaskAddForm = ({addTask}: IAddTask) => {
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
@@ -47,7 +55,7 @@ const TaskAddingForm = ({addTask}: IAddTask) => {
         };
 
         addTask(
-            <TaskListElement
+            <TaskListEntry
                 initTaskName={target.taskName.value}
                 initTaskDescription={target.taskDescription.value}
                 initTaskID="TASKID"/>
@@ -63,29 +71,20 @@ const TaskAddingForm = ({addTask}: IAddTask) => {
     );
 }
 
-const TaskSearch = () => {
+
+const TaskSearchForm = () => {
     return (
-        <div className="task_search">
-            <h1>Search</h1>
-            <form className="task_search_form actions">
-                <input type="text" className="task_search_form_input" placeholder="..."/>
-                <input type="submit" className="task_search_form_submit" value="Search"/>
-            </form>
-        </div>
+        <form className="task_search_form actions">
+            <input type="text" className="task_search_form_input" placeholder="..."/>
+            <input type="submit" className="task_search_form_submit" value="Search"/>
+        </form>
     );
 }
 
-const TaskManageBar = ({addTask}: IAddTask) => {
-    return (
-        <div className="task_manage_bar">
-            <TaskAddingForm
-                addTask={addTask}/>
-            <h1>LETS GO</h1>
-            <TaskSearch/>
-        </div>
-    );
-}
 
+interface ITasksArr {
+    tasksList: JSX.Element[]
+}
 const TaskList = ({tasksList}: ITasksArr) => {
     console.log(tasksList[0]);
     return (
@@ -104,24 +103,31 @@ const TaskList = ({tasksList}: ITasksArr) => {
     );
 }
 
-const TodoApp = () => {
-    const [tasksList, setTasksList] = useState<JSX.Element[]>([]);
 
-    const addTask = (task: JSX.Element) => {
-        setTasksList([
-            ...tasksList,
-            task
-        ]);
-    }
+interface ITaskListElement {
+    initTaskName: string,
+    initTaskDescription: string,
+    initTaskID: string
+}
+const TaskListEntry = ({initTaskName, initTaskDescription, initTaskID}: ITaskListElement) => {
+    const [taskID, setTaskID] = useState(initTaskID);
+    const [taskName, setTaskName] = useState(initTaskName);
+    const [taskDescription, setTaskDescription] = useState(initTaskDescription);
 
     return (
-        <div className="todo_app">
-            <TaskManageBar
-                addTask={addTask}/>
-            <TaskList
-                tasksList={tasksList}/>
+        <div className="task_list_element">
+            <div className="content">
+                <input type="text" className="text" value={taskID} readOnly/>
+                <input type="text" className="text" value={taskName} readOnly/>
+                <input type="text" className="text" value={taskDescription} readOnly/>
+            </div>
+            <div className="actions">
+                <button className="edit">edit</button>
+                <button className="delete">delete</button>
+            </div>
         </div>
     );
 }
+
 
 export default TodoApp;
