d9b6389470d637cdf9e4c59e7ead6ed16bf0722d
ambid3xter <plexalex3@gmail.com>
2023-04-24 16:11:18 +1000

    added comments to authorization.py

--- flask-backend/authorization.py
+++ flask-backend/authorization.py
@@ -9,15 +9,15 @@ db = client['db_sample']
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
-@app.route('/')
+@app.route('/') #Функция index() обрабатывает GET запрос на главную страницу.если есть
 def index():
-    if 'username' in session:
-        return render_template('index.html', username=session['username'], balance=session['balance'])
+    if 'username' in session: #проверяет, есть ли в текущей сессии сохраненный параметр "username"
+        return render_template('index.html', username=session['username'], balance=session['balance']) #если есть, передает его и "balance" в качестве аргументов в шаблон index.html
 
     return render_template('index.html')
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST']) #Функция login() использует объект "db" для получения доступа к базе данных, где хранятся зарегистрированные пользователи.
 def login():
     users = db.users
     login_user = users.find_one({'name': request.form['username']})
@@ -25,24 +25,24 @@ def login():
     if login_user:
         if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
-            session['balance'] = login_user['balance']
+            session['balance'] = login_user['balance'] #Для сравнения паролей используется метод hashpw() из библиотеки bcrypt. Если пароли совпадают, то пользователь считается аутентифицированным и сохраняются его имя и баланс в сессию.
             return redirect(url_for('index'))
 
         return 'Неправильно введен логин или пароль'
 
 
 @app.route('/register', methods=['POST', 'GET'])
-def register():
+def register(): #извлекает список всех зарегистрированных пользователей из коллекции "users" в базе данных, затем проверяет, есть ли уже пользователь с таким же именем
     if request.method == 'POST':
         users = db.users
         exisiting_user = users.find_one({'name': request.form['username']})
 
-        if exisiting_user is None:
+        if exisiting_user is None: #если такого пользователя еще нет, то функция генерирует хэш от введенного пароля, используя библиотеку bcrypt, и сохраняет нового пользователя в базе данных.
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
             users.insert_one(
                 {'name': request.form['username'], 'password': hashpass, 'balance': int(request.form['balance'])})
             session['username'] = request.form['username']
-            session['balance'] = int(request.form['balance'])
+            session['balance'] = int(request.form['balance']) #Затем, функция сохраняет имя и баланс нового пользователя в сессию, используя объект session
             return redirect(url_for('index'))
 
         return 'Такое имя пользователя уже существует'
