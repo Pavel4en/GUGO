a94ec94fe302baccd4b1ac2b21b933136453c789
ambid3xter <plexalex3@gmail.com>
2023-04-24 15:50:19 +1000

    added balance to users

--- flask-backend/authorization.py
+++ flask-backend/authorization.py
@@ -12,7 +12,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 @app.route('/')
 def index():
     if 'username' in session:
-        return 'Вы авторизованы как: ' + session['username']
+        return render_template('index.html', username=session['username'], balance=session['balance'])
 
     return render_template('index.html')
 
@@ -25,6 +25,7 @@ def login():
     if login_user:
         if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
+            session['balance'] = login_user['balance']
             return redirect(url_for('index'))
 
         return 'Неправильно введен логин или пароль'
@@ -38,8 +39,10 @@ def register():
 
         if exisiting_user is None:
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert_one({'name': request.form['username'], 'password': hashpass})
+            users.insert_one(
+                {'name': request.form['username'], 'password': hashpass, 'balance': int(request.form['balance'])})
             session['username'] = request.form['username']
+            session['balance'] = int(request.form['balance'])
             return redirect(url_for('index'))
 
         return 'Такое имя пользователя уже существует'
